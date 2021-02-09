45ac1433ac9eb51b10646928e1597719028ac50a
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-08 19:15:14 -0500

    Create 0002.hs

---
+++ 0002/0002.hs
@@ -0,0 +1,12 @@
+theSquirts :: Int -> Int;
+theSquirts k
+    | k < 2^29-1 = floor $ sqrt $ fromIntegral k
+    | otherwise  = read $ take (length b `div` 2 + 1) $ b
+        where b = show k
+
+isPrime :: Int -> Bool;
+isPrime = (== []) . factors
+  where factors k = [l | l <- [2..theSquirts k], mod k l == 0]
+
+main :: IO [()];
+main = mapM print $ filter isPrime [2..];
