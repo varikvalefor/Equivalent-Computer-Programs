e817fba72514a94ef0072e63f1b71c39d1154cfe
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-13 22:24:52 -0500

    Int becomes Integer in 0002.hs.

--- 0002/0002.hs
+++ 0002/0002.hs
@@ -1,12 +1,13 @@
-theSquirts :: Int -> Int;
+theSquirts :: Integer -> Integer;
 theSquirts k
     | k < 2^29-1 = floor $ sqrt $ fromIntegral k
-    | otherwise  = read $ take (length b `div` 2 + 1) $ b
-        where b = show k
+    | otherwise  = read $ take (length b `div` 2 + 1) b
+      where b = show k
 
-isPrime :: Int -> Bool;
+isPrime :: Integer -> Bool;
 isPrime = (== []) . factors
   where factors k = [l | l <- [2..theSquirts k], mod k l == 0]
 
 main :: IO [()];
 main = mapM print $ filter isPrime [2..];
+
