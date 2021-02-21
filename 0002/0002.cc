a5145abdc8fc71109ec9f18d5089dd3e7df21c38
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-21 13:18:17 -0500

    The length of 0002.cc is reduced.

--- 0002/0002.cc
+++ 0002/0002.cc
@@ -4,8 +4,7 @@
 bool isPrime(mpz_class n)
 {
 	mpz_class m,o,p;
-	o = sqrt(n);
-	for(m = 2; m < o; m++)
+	for(m = 2; m < sqrt(n); m++)
 		if(n % m == 0)
 			return false;
 	return true;
@@ -13,7 +12,7 @@ bool isPrime(mpz_class n)
 
 int main()
 {
-	for(mpz_class n = 2; n > 0; n++)
+	for(mpz_class n = 2 ;; n++)
 		if(isPrime(n))
 			std::cout << n << "\n";
 }
