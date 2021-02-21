07061ce972788634325c3301e9833dd63605bd4a
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-20 20:33:07 -0500

    0006.lisp now generates an infinite set of pseudorandoms.

--- 0006/0006.lisp
+++ 0006/0006.lisp
@@ -1,5 +1,6 @@
 ;This program is written with VIM.  Haw-haw!
 (DEFUN K () (MAKE-RANDOM-STATE T))
-(FORMAT T "~d"
-	(* (RANDOM (EXPT 2 63) (K))
-		 (IF (EQUAL (RANDOM 2 (K)) 0) -1 1)))
+(LOOP WHILE (EQUAL 1 1) DO
+	(PRINT
+		(* (RANDOM (EXPT 2 63) (K))
+			 (IF (EQUAL (RANDOM 2 (K)) 0) -1 1))))
