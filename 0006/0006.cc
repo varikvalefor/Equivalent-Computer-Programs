da67a76eaf0049d707f2f53889fee02e729238fd
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-02-21 13:11:24 -0500

    0006.cc now returns some negatives... and values of the correct bittage.

--- 0006/0006.cc
+++ 0006/0006.cc
@@ -1,13 +1,20 @@
 #include <gmpxx.h>
 #include <iostream>
 #include <stdlib.h>
+#define G arc4random()
+
+mpz_class calcSign()
+{
+	if(G % 2 == 1) return 1;
+	else           return -1;
+}
 
 int main()
 {
 	gmp_randclass x (gmp_randinit_default);
 	while(1)
 	{
-		x.seed(arc4random() * arc4random());
-		std::cout << x.get_z_bits(4096) << "\n";
+		x.seed(G*G);
+		std::cout << x.get_z_bits(63) * calcSign() << "\n";
 	}
 }
