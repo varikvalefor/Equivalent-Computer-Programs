8194c52b2ae50da5b5be506aca5dcdc1b8a7f87f
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-27 23:53:47 -0400

    The length of 0022.cc decreases.

--- 0022/0022.cc
+++ 0022/0022.cc
@@ -5,11 +5,10 @@ int main()
 {
 	std::cout.setf(std::ios::unitbuf);
 
-	for(char j : "We are the music makers.")
+	for(char j : "We are the music makers.\n")
 	{
 		std::cout << j;
 		nanosleep((const struct timespec []) {0,100000000}, NULL);
 	}
-	std::cout << std::endl;
 	main();
 }
