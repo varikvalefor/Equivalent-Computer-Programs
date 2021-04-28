6d9e18348e5e16651b164630606b41439c2fe280
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-27 23:53:10 -0400

    The length of 0022.hs decreases.

--- 0022/0022.hs
+++ 0022/0022.hs
@@ -2,4 +2,4 @@ import Control.Concurrent;
 
 main :: IO ();
 main = mapM_ (\k -> putStr [k] >> threadDelay 100000)
-  "We are the music makers." >> putStrLn "" >> main;
+  "We are the music makers.\n" >> main;
