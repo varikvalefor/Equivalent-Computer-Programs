4658199bf6aba2e1e7453e0912486b1e571d44c0
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-04 22:31:26 -0400

    0003.cc receives an explanation.

--- 0003/0003.cc
+++ 0003/0003.cc
@@ -5,6 +5,9 @@
 int main(int argc, char ** argv)
 {
 	for(mpz_class a = 1; a <= mpz_class(argv[1], 10); a++)
+		/* mpz_class(argv[1], 10) converts the first argument of
+		0023 to a value of type mpz_class; such conversion is a
+		prerequisite of comparing this argument and variable a. */
 		std::cout << a;
 	std::cout << std::endl;
 	return(0);
