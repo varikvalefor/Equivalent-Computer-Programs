6443938f903b81d51ebef9f508acd98d263cea57
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-04 22:27:30 -0400

    0023.cc receives an explanation.

--- 0023/0023.cc
+++ 0023/0023.cc
@@ -5,7 +5,16 @@
 void listThePhiles(char * d)
 {
 	for(auto f : std::filesystem::directory_iterator(d))
+		/* f iterates over std::filesystem::directory_iterator(d), which
+		contains a list of the files of directory d. */
 		std::cout << f.path().filename().string() << std::endl;
+		/* Oh, boy...
+		f.path() is the full file path of f.
+		f.path() contains the parent directories of f, and these results are
+		to be discarded. As such, f.path().filename() returns the
+		directory-stripped filename of f; however, f.path.filename() is not a
+		string.  Because a string is desired, the string() function of
+		f.path().filename() is used to output a string. */
 }
 
 int main(int argc, char ** argv)
