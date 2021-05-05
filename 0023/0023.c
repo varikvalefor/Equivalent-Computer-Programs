bfecf29e2bf4727bec70877a2af5a77dd14a03e2
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-04 22:20:51 -0400

    0023.c receives some explanations.

--- 0023/0023.c
+++ 0023/0023.c
@@ -7,14 +7,15 @@ void listOf(char * k)
   DIR *toLS = opendir(k);
   if(toLS)
   {
-    for(int i = 0; i < 2; i++)
-      readdir(toLS);
-    while((x = readdir(toLS)) != NULL)
-      printf("%s\n", x->d_name);
+    for(int i = 0; i < 2; i++) // The first two (2) entries,
+      readdir(toLS);           // ../ and ./, are junked.
+    while((x = readdir(toLS)) != NULL) // The program stops after reading all
+      printf("%s\n", x->d_name);       // directory entries.
     closedir(toLS);
   }
   else
     perror("1");
+  //The program throws a fit if an invalid directory is specified.
 }
 
 int main(int argc, char ** argv)
