6612e108b171b151486a2f0627c0843a2500857f
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-04 22:35:03 -0400

    0003.hs receives an explanation.

--- 0003/0003.hs
+++ 0003/0003.hs
@@ -3,3 +3,10 @@ import System.Environment;
 main :: IO ();
 main = getArgs >>= \a ->
   putStrLn $ foldr (++) [] $ map (show) [1..read (a !! 0)];
+  {- [1..read (a !! 0)] generates a list of all integers between
+  1 and the first argument of the program.
+  map (show) converts the aforementioned list of integers into a
+  list of decimal representations of these integers.
+  foldr (++) [] performs a rightward concatenation on the resulting
+  [String].
+  putStrLn prints this String to the console. -}
