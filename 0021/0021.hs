2cdb0f85877aee9464894d685ef49dd92f1663cd
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-08 21:29:57 -0400

    0021.hs receives an explanation.

--- 0021/0021.hs
+++ 0021/0021.hs
@@ -2,3 +2,8 @@ import System.Environment;
 
 main :: IO ();
 main = getEnvironment >>= mapM_ (\a -> putStrLn $ fst a ++ "=" ++ snd a);
+{- For all K in getEnvironment, K is a 2-sequence of the name of an
+environment variable and the value of this evironment variable.
+
+For all k in getEnvironment, "=" is placed between (fst k) and (snd k);
+the resulting string is printed to the console. -}
