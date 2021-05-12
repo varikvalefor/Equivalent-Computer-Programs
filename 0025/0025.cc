de9981200caecb21e966cc03a9e9e6210b07f057
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-05-11 20:28:42 -0400

    Create 0025.cc

---
+++ 0025/0025.cc
@@ -0,0 +1,20 @@
+#include <gmpxx.h>
+#include <iostream>
+
+mpz_class faktorial(mpz_class g)
+{
+	if(g > 1) return g * faktorial(g - 1);
+	else      return g;
+	/* The author elects to use recursion instead of a "for" loop; the
+	 * author finds that the conciseness of the resulting recursion is
+	 * greater than the conciseness of the alternative to the resulting
+	 * recursion. */
+}
+
+int main()
+{
+	for(mpz_class a = 1 ;; a++)
+		std::cout << faktorial(a) << std::endl;
+	/* For all natural numbers a, std::cout outputs faktorial(a), i.e.,
+	 * the factorial of natural number a. */
+}
