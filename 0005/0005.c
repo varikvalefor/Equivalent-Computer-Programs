6ade4375b65ce602f8db154ffd967d2b6bc4fe86
Varik Valefor <varikvalefor@aol.com>
2021-09-15 19:42:33 -0400

    0005.c: A "for" loop is explained.

--- 0005/0005.c
+++ 0005/0005.c
@@ -7,6 +7,10 @@ int main(int argc, char * * argv)
 	mpz_inits(a,b);
 	mpz_set_str(a,argv[1],10);
 
+	/* The following line is roughly equivalent to
+	"for(b = 1; b < a; b++)".  However, C's GMP does not support
+	the ++ operator; as such, the use of GMP-specific functions is
+	required for proper operation. */
 	for(mpz_set_ui(b,1); mpz_cmp(a,b) >= 0; mpz_add_ui(b,b,1))
 		gmp_printf("%Zd", b);
 	printf("\n");
