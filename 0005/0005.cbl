be18e36b623920a0b9014361fafe72ffbb7673e6
Varik Valefor <varikvalefor@aol.com>
2021-09-15 19:39:18 -0400

    0005.cbl: The use of "NO ADVANCING" is explained.

--- 0005/0005.cbl
+++ 0005/0005.cbl
@@ -16,6 +16,10 @@
            ADD 1 TO CURRENTX.
            MOVE CURRENTX TO CURRENTY.
            DISPLAY FUNCTION TRIM (CURRENTY LEADING) WITH NO ADVANCING.
+      *    "NO ADVANCING" removes the newline which would otherwise be
+      *    added to the output; such a newline is unnecessary... and
+      *    separates individual numbers, which is not the desired
+      *    behaviour.
            IF (CURRENTX + 1) <= BOUNDARY THEN
              GO TO PARA-PARA
            END-IF.
