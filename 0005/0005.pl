de5503ada15ad19ce10eef91cd5d18c510c2862f
Varik Valefor <varikvalefor@aol.com>
2021-09-15 19:58:45 -0400

    0005.pl is now written in the functional style.

--- 0005/0005.pl
+++ 0005/0005.pl
@@ -1,12 +1,17 @@
-$maximumNumber = int @ARGV[0];
-$concatenatedNumbers = "";
-$currentNum = 1;
-
-# For all $currentNum between 1 and $maximumNumber...
-for ($currentNum = 1; $currentNum <= $maximumNumber; $currentNum++)
+sub concatenateNumbers
 {
-	# $currentNum is appended to $concatenatedNumbers.
-	$concatenatedNumbers = "$concatenatedNumbers$currentNum";
+	my $maximumNumber = shift;
+	my $concatenatedNumbers = "";
+	my $currentNum = 0;
+
+	# For all $currentNum between 1 and $maximumNumber...
+	for(my $currentNum = 1; $currentNum <= $maximumNumber; $currentNum++)
+	{
+		# $currentNum is appended to $concatenatedNumbers.
+		$concatenatedNumbers = "$concatenatedNumbers$currentNum";
+	}
+
+	return $concatenatedNumbers;
 }
 
-print "$concatenatedNumbers\n";
+print concatenateNumbers(@ARGV[0]), "\n";
