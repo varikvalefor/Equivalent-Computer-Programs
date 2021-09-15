27f122713cd6f847cfaed1b9ead19011698c9659
Varik Valefor <varikvalefor@aol.com>
2021-09-15 19:49:21 -0400

    0005.pl: The "for" loop is explained.

--- 0005/0005.pl
+++ 0005/0005.pl
@@ -2,8 +2,10 @@ $maximumNumber = int @ARGV[0];
 $concatenatedNumbers = "";
 $currentNum = 1;
 
+# For all $currentNum between 1 and $maximumNumber...
 for ($currentNum = 1; $currentNum <= $maximumNumber; $currentNum++)
 {
+	# $currentNum is appended to $concatenatedNumbers.
 	$concatenatedNumbers = "$concatenatedNumbers$currentNum";
 }
 
