81fe20af4b38e0eb840a0fcfbd7cbddd08c17c40
Varik Valefor <varikvalefor@aol.com>
2021-09-15 20:17:54 -0400

    0022.pl is explained.

--- 0022/0022.pl
+++ 0022/0022.pl
@@ -2,13 +2,20 @@
 
 use Time::HiRes;
 
+# Buffering is disabled, ensuring that characters are "immediately"
+# written to the standard output.
 $|=1;
 
+# This loop is run forever... unless something goes horribly, horribly
+# wrong.
 while(true)
 {
+	# For all characters in the message which should be written...
 	foreach $i (split //, "We are the music makers.\n")
 	{
+		# A character is written to the standard output.
 		print($i);
+		# The program sleeps for 0.1 seconds.
 		Time::HiRes::usleep(100000);
 	}
 }
