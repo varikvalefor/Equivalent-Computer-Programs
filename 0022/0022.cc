ccc4c10bd0d0458e384740973d73c15491673586
Varik Valefor <varikvalefor@aol.com>
2021-09-15 20:10:29 -0400

    0022.cc: s/  /\t/

--- 0022/0022.cc
+++ 0022/0022.cc
@@ -10,7 +10,7 @@ int main()
 	/* For all characters in the string... */
 	for(char j : "We are the music makers.\n")
 	{
-	  /* The current character is written to the standard output. */
+		/* The current character is written to the standard output. */
 		std::cout << j;
 		/* The program sleeps for 0.1 seconds before repeating the loop. */
 		nanosleep((const struct timespec []) {0,100000000}, NULL);
