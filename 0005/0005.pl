bdfa08a555cba7309044cc959a7097f192b261b3
Varik Valefor <varikvalefor@aol.com>
2021-09-15 20:00:52 -0400

    0005.pl: Documentation is improved.

--- 0005/0005.pl
+++ 0005/0005.pl
@@ -1,7 +1,14 @@
+# concatenateNumbers(j) is the concatenation of all numbers between 1
+# and j.
 sub concatenateNumbers
 {
+	# $maximumNumber is the maximum number which should be added to the
+	# concatenated thing.
 	my $maximumNumber = shift;
+	# $concatenatedNumbers is the current string of concatenated numbers.
 	my $concatenatedNumbers = "";
+	# $currentNum is the number which is currently being added to the
+	# string of concatenated numbers.
 	my $currentNum = 0;
 
 	# For all $currentNum between 1 and $maximumNumber...
