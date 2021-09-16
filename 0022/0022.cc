0a778933f11b3a09fc1ff5258751e168b496c952
Varik Valefor <varikvalefor@aol.com>
2021-09-15 20:10:08 -0400

    0022.cc: Some explanations are added.

--- 0022/0022.cc
+++ 0022/0022.cc
@@ -3,11 +3,16 @@
 
 int main()
 {
+	/* This thing disables buffering, permitting the writing of individual
+	   characters to the standard output. */
 	std::cout.setf(std::ios::unitbuf);
 
+	/* For all characters in the string... */
 	for(char j : "We are the music makers.\n")
 	{
+	  /* The current character is written to the standard output. */
 		std::cout << j;
+		/* The program sleeps for 0.1 seconds before repeating the loop. */
 		nanosleep((const struct timespec []) {0,100000000}, NULL);
 	}
 	main();
