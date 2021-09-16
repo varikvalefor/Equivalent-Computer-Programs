7b18ade19ffd39080d5c1439705a36fc32c62a80
Varik Valefor <varikvalefor@aol.com>
2021-09-15 20:30:46 -0400

    0025.c: Some comments are moved a bit.

--- 0025/0025.c
+++ 0025/0025.c
@@ -5,11 +5,11 @@ void faq(mpz_t result, mpz_t k)
 {
 	mpz_t x;
 	mpz_set_ui(x, 1);
+	/* The following line is roughly equivalent to
+	 * "for(int result = 1; x < k; k++)". */
 	for(mpz_init_set_ui(result, 1); mpz_cmp(x, k) <= 0; mpz_add_ui(x,x,1))
-		/* The above line is roughly equivalent to
-		 * "for(int result = 1; x < k; k++)". */
-		mpz_mul(result, result, x);
 		/* The product of result and x is assigned to result. */
+		mpz_mul(result, result, x);
 	/* When the "for" loop ends, "result" is already defined as being the
 	 * factorial -- no further commands are necessary.  So ends this
 	 * comment. */
@@ -19,13 +19,13 @@ int main()
 {
 	mpz_t a, currentFactorialResult, l;
 	mpz_init(currentFactorialResult);
-	for(mpz_init_set_ui(a, 0) ;; mpz_add_ui(a, a, 1))
-	/* The above line is roughly equivalent to "for(int a = 0 ;; a++)".
+	/* The following line is roughly equivalent to "for(int a = 0 ;; a++)".
 	For all natural numbers a, bracketed bit is "done" on a. */
+	for(mpz_init_set_ui(a, 0) ;; mpz_add_ui(a, a, 1))
 	{
-		faq(currentFactorialResult, a);
 		/* The factorial of a is assigned to currentFactorialResult. */
-		gmp_printf("%Zd\n", currentFactorialResult);
+		faq(currentFactorialResult, a);
 		/* currentFactorialResult is output to the terminal. */
+		gmp_printf("%Zd\n", currentFactorialResult);
 	}
 }
